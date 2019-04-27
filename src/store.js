afd6d6783f30335969a2cf781bf00229c7130b9f
Mariyan Tenev <rggf132@gmail.com>
2019-04-27 15:38:27 +0100

    Ensure redux uses store config file

--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers/index';
+
+const sagaMiddleware = createSagaMiddleware()
+
 export default function configureStore(initialState={}) {
+    const composeEnhancers =
+        typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+                // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+            }) : compose;
+
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composeEnhancers(
+            applyMiddleware(sagaMiddleware)
+        )
     );
 }
