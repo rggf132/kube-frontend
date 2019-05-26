d080be883b19bfd9638437f7cf25beffdc76b6f8
Mariyan Tenev <rggf132@gmail.com>
2019-05-26 22:19:43 +0100

    Added Log in button, with Router Links to navigate to Login Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { DatePicker } from 'antd';
-import './App.css'
-import 'antd/dist/antd.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { DatePicker } from "antd";
+import "./App.css";
+import "antd/dist/antd.css";
 import Tasks from "./Tasks";
+import Login from "./Login";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    Task Application
-                </div>
-                <Tasks/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">Task Application</div>
+        <div style={{ position: "fixed", top: 5, left: 5 }}>
+          <Link to="/login">
+            <Button type="primary">Log in</Button>
+          </Link>
+        </div>
+        <Tasks />
+      </div>
+    );
+  }
 }
 
 export default App;
