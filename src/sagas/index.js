8b1eac610f6ef330d0d3aa182575a08c0a37c74f
Mariyan Tenev <rggf132@gmail.com>
2019-05-26 22:24:35 +0100

    Added login saga to rootSage / index

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,8 @@
-import { fork } from "redux-saga/effects"
-import task from "./task"
+import { fork } from "redux-saga/effects";
+import task from "./task";
+import user from "./user";
 
 export function* rootSaga() {
-    yield fork(task)
+  yield fork(task);
+  yield fork(user);
 }
