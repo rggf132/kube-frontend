5e0d4b9fc828a057507d32932b59572aa115fd5c
Mariyan Tenev <rggf132@gmail.com>
2019-06-13 22:08:43 +0100

    Also, used push() to go back to the tasks. Changed HOST address to include '/api', just as with other saga.

--- src/sagas/user.js
+++ src/sagas/user.js
@@ -1,9 +1,10 @@
 import { call, put, takeEvery, select } from "redux-saga/effects";
 import { loginSuccess, loginFailure } from "../actions/user";
+import { push } from "connected-react-router";
 import axios from "axios";
 
 // const HOST = "http://35.189.87.15:3000";
-const HOST = "http://localhost:3000";
+const HOST = "http://localhost:3001/api";
 
 const ENDPOINT_USER = "/auth/local";
 
@@ -18,6 +19,7 @@ function* login(action) {
     );
 
     yield put(loginSuccess(response));
+    yield put(push("/"));
   } catch (error) {
     yield put(loginFailure(error));
   }
